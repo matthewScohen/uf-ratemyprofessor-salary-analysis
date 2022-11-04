21917ff05db9df8804bc2089a3e18c20babc1a0d
Matthew Cohen <cohen.matthew@ufl.edu>
2022-11-03 22:47:17 -0400

    limit the number of times we try to load a page if it fails to load, print the professors it fails to load at the end of the script

--- src/collect_gator_eval_tableau.py
+++ src/collect_gator_eval_tableau.py
@@ -25,31 +25,40 @@ for term in terms:
         professor_names = filters[3]["values"][1:None] 
     else:
         professor_names = filters[3]["values"]
+        
     professors_done = 0
+    uncollected_professors = [] # a list of professor's the script failed to collect data on
     for professor in professor_names:
         # reset worksheet (see comment above)
-        while True:
+        attempts = 0
+        data_loaded = False
+        while attempts < 5:
             try:
                 ts.loads(url)
+                attempts += 1
             except Exception e:
                 print(f"error collecting {url}: {e}")
             else:
                 break
-        workbook = ts.getWorkbook()
-        ws = workbook.worksheets[0]
-        wb = ws.setFilter("INSTRUCTOR_NAME", professor)
-        # get courses
-        filters = ws.getFilters()
-        courses = filters[1]["values"]
-        # print(courses)
-        for course in courses:    
-            wb = ws.setFilter("COMBINED_COURSE", course)
-            wb.worksheets[0].data["professor"] = professor
-            wb.worksheets[0].data["term"] = term
-            wb.worksheets[0].data["course"] = course
-            df = pd.concat([df, wb.worksheets[0].data])
-        professors_done += 1
-        print(f"Collecting data for {term}... {professors_done/len(professor_names):.2f}%")
+        if data_loaded:
+            workbook = ts.getWorkbook()
+            ws = workbook.worksheets[0]
+            wb = ws.setFilter("INSTRUCTOR_NAME", professor)
+            # get courses
+            filters = ws.getFilters()
+            courses = filters[1]["values"]
+            # print(courses)
+            for course in courses:    
+                wb = ws.setFilter("COMBINED_COURSE", course)
+                wb.worksheets[0].data["professor"] = professor
+                wb.worksheets[0].data["term"] = term
+                wb.worksheets[0].data["course"] = course
+                df = pd.concat([df, wb.worksheets[0].data])
+            professors_done += 1
+            print(f"Collecting data for {term}... {professors_done/len(professor_names):.2f}%")
+        else:
+            uncollected_professors.append(professor)
 
+print(f"Failed to collect data for these professors: {uncollected_professors}")
 with open("new_gator_eval_data.pickle", "wb") as handle:
         pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
